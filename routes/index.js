23502d9f9110d21b9f643c10df85fe5e9b6536b9
Aaron Bellach <aaronbellach@Aarons-MacBook-Pro.local>
2020-11-23 13:19:39 -0800

    setup boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+const passport = require('passport');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// The root route renders our only view
+router.get('/', function(req, res) {
+  // Where do you want to go for the root route
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/', // where do you want the client to go after you login 
+    failureRedirect : '/' // where do you want the client to go if login fails
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
